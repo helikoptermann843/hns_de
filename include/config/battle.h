5a1debe7673b687a9d04c60d518541b25c707448
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-06-22 21:14:29 -0400

    Badge boost changes

--- include/config/battle.h
+++ include/config/battle.h
@@ -251,7 +251,11 @@
 
 // Badge boost flags
 #define B_FLAG_BADGE_BOOST_ATTACK   FLAG_BADGE01_GET // If this flag is set and B_BADGE_BOOST == GEN_3, it will multiply the player's Pokémon's Attack by x1.1
-#define B_FLAG_BADGE_BOOST_DEFENSE  FLAG_BADGE05_GET // If this flag is set and B_BADGE_BOOST == GEN_3, it will multiply the player's Pokémon's Defense by x1.1
+#if IS_HNS
+#define B_FLAG_BADGE_BOOST_DEFENSE  FLAG_BADGE06_GET // HnS: Mineral Badge (Jasmine) boosts Defense
+#else
+#define B_FLAG_BADGE_BOOST_DEFENSE  FLAG_BADGE05_GET // Emerald: Balance Badge (Norman) boosts Defense
+#endif
 #define B_FLAG_BADGE_BOOST_SPEED    FLAG_BADGE03_GET // If this flag is set and B_BADGE_BOOST == GEN_3, it will multiply the player's Pokémon's Speed by x1.1
 #define B_FLAG_BADGE_BOOST_SPATK    FLAG_BADGE07_GET // If this flag is set and B_BADGE_BOOST == GEN_3, it will multiply the player's Pokémon's Sp. Atk by x1.1
 #define B_FLAG_BADGE_BOOST_SPDEF    FLAG_BADGE07_GET // If this flag is set and B_BADGE_BOOST == GEN_3, it will multiply the player's Pokémon's Sp. Def by x1.1
